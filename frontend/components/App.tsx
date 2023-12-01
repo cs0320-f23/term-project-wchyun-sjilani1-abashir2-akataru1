5c9fd2473622d3234c13bda4c91237e0de872af7
Merzia Jilani <merziajilani@merziasmbp1590.devices.brown.edu>
2023-12-01 14:48:47 -0500

    feat: dropbox rough work

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,18 +1,45 @@
 import React, { useEffect, useState } from "react";
 import "../styles/App.css";
-import "./MapDisplay/MapBox";
-import MapBox from "./MapDisplay/MapBox";
+
+
 
 // REMEMBER TO PUT YOUR API KEY IN A FOLDER THAT IS GITIGNORED!!
 // (for instance, /src/private/api_key.tsx)
 // import {API_KEY} from "./private/api_key"
 
 function App() {
-  return <div className="App">
-    {
-    <MapBox />   
-    }
+
+  const[open, setOpen] = useState(false); 
+
+  return ( <div className="App">
+    <div className="menu-container"> 
+    <div className="menu-trigger" onClick={()=>{setOpen(!open)}}>
+    </div>
+    <div className="dropdown-menu"> </div>
+    <h3>Date Selection</h3>
+    <ul> <DropdownItem/>
+    <DropdownItem  text = {"Monday"} /> 
+    <DropdownItem text = {"Tuesday"}/> 
+    <DropdownItem text = {"Wednesday"}/> 
+    <DropdownItem text = {"Thursday"}/> 
+    <DropdownItem text = {"Friday"}/> 
+    <DropdownItem text = {"Saturday"}/>  
+    <DropdownItem text = {"Sunday"}/>  
+    </ul>
+    
+    </div>
+  
   </div>
+  );
+}
+
+function DropdownItem(props) { 
+  return( 
+    <li className = 'dropdownItem'> 
+    <a> [props.text] </a>
+    </li>
+
+  ); 
 }
 
 export default App;
