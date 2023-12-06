6be85aa00d8b05c1382c2c2166993ab72160fcb7
Merzia Jilani <merziajilani@merziasmbp1737.devices.brown.edu>
2023-12-05 20:33:40 -0500

    feat: Home

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -7,39 +7,8 @@ import "../styles/App.css";
 // (for instance, /src/private/api_key.tsx)
 // import {API_KEY} from "./private/api_key"
 
-function App() {
+function App() { }
 
-  const[open, setOpen] = useState(false); 
-
-  return ( <div className="App">
-    <div className="menu-container"> 
-    <div className="menu-trigger" onClick={()=>{setOpen(!open)}}>
-    </div>
-    <div className="dropdown-menu"> </div>
-    <h3>Date Selection</h3>
-    <ul> <DropdownItem/>
-    <DropdownItem  text = {"Monday"} /> 
-    <DropdownItem text = {"Tuesday"}/> 
-    <DropdownItem text = {"Wednesday"}/> 
-    <DropdownItem text = {"Thursday"}/> 
-    <DropdownItem text = {"Friday"}/> 
-    <DropdownItem text = {"Saturday"}/>  
-    <DropdownItem text = {"Sunday"}/>  
-    </ul>
-    
-    </div>
   
-  </div>
-  );
-}
-
-function DropdownItem(props) { 
-  return( 
-    <li className = 'dropdownItem'> 
-    <a> [props.text] </a>
-    </li>
-
-  ); 
-}
 
 export default App;
